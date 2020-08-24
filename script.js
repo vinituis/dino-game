3708fce4f3005c5d5074fec3807905194e5d7532
vini <vinicius.turu@gmail.com>
2020-08-23 23:47:28 -0300

    Funcionalidade do Jogo Finaizada

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
@@ -10,8 +12,7 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-    let position = 0;
-
+    
     isJumping = true;
 
     let upInterval = setInterval(() => {
@@ -35,5 +36,33 @@ function jump() {
         }
     }, 20);
 }
+
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if(cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60 ) {
+            //Game over
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim de Jogo</h1>';
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
 
 document.addEventListener('keyup', handleKeyUp);
