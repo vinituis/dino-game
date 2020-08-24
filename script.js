d31ff129e085216d421789af1483c11d60dcd3f5
vini <vinicius.turu@gmail.com>
2020-08-23 23:17:08 -0300

    Pulo do dino finalizado

---
+++ script.js
@@ -0,0 +1,39 @@
+const dino = document.querySelector('.dino');
+let isJumping = false;
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        if (!isJumping) {
+            jump();
+        }
+    }
+}
+
+function jump() {
+    let position = 0;
+
+    isJumping = true;
+
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+
+            //descendo
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                    isJumping = false;
+                } else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        } else {
+            //subindo
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    }, 20);
+}
+
+document.addEventListener('keyup', handleKeyUp);
